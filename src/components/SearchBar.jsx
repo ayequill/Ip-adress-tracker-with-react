0c3aa79cf0d0d2779a841c06041a03a5de8d202a
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-04-23 19:13:26 +0000

    Fix search responsiveness

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,28 +4,30 @@ import {
   InputGroup,
   Input,
   InputRightAddon,
-  Icon,
+  Icon, Flex,
 } from '@chakra-ui/react';
 import { FaChevronRight } from 'react-icons/fa';
 
 function SearchBar() {
   return (
-    <Box
+    <Flex
       as="header"
-      height="16em"
+      height="12em"
       backgroundSize="cover"
       backgroundRepeat="no-repeat"
       backgroundPosition="center"
       backgroundImage={{ base: 'src/assets/pattern-bg-mobile.png', md: 'src/assets/pattern-bg-desktop.png' }}
+      alignItems="center"
+      justifyContent="center"
     >
-      <Box p={4} display="grid">
-        <Heading color="white" textAlign="center" fontSize={{ base: '1.7rem', md: 'sm' }}>IP Address Tracker</Heading>
-        <InputGroup size="lg" mt="10">
-          <Input borderRadius=".7em" backgroundColor="white" placeholder="Search for any IP address or domain" border="rgb(87,116,215) solid 1px" />
+      <Box display="grid" w={{ base: '80%', md: '50%' }}>
+        <Heading color="white" textAlign="center" fontSize={{ base: '1.7rem' }}>IP Address Tracker</Heading>
+        <InputGroup size="lg" mt="8">
+          <Input w="100%" borderRadius=".7em" backgroundColor="white" placeholder="Search for any IP address or domain" border="rgb(87,116,215) solid 1px" />
           <InputRightAddon border="none" backgroundColor="blackAlpha.900"><Icon color="white" as={FaChevronRight} /></InputRightAddon>
         </InputGroup>
       </Box>
-    </Box>
+    </Flex>
   );
 }
 
