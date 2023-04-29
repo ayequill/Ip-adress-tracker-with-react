14297121356c0a56cca430c5481f0a445b955136
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-04-29 02:31:17 +0000

    Wrapped the map in container

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { MapContainer, TileLayer } from 'react-leaflet';
+import { Box } from '@chakra-ui/react';
 import MarkerPosition from './MarkerPosition';
 
 const MapContainers = ({ address }) => (
-  <>
+  <Box>
     {address
     && (
-    <>
       <MapContainer center={[address.location.lat, address.location.lng]} zoom={13} scrollWheelZoom style={{ height: '700px', width: '100vw' }}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -15,9 +15,8 @@ const MapContainers = ({ address }) => (
         />
         <MarkerPosition address={address} />
       </MapContainer>
-    </>
     )}
-  </>
+  </Box>
 );
 
 MapContainers.propTypes = {
