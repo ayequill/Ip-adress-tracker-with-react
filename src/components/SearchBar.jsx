6210a38cd66b49f1fe978cbc92eb696c8cc153df
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-04-29 02:30:48 +0000

    FEAT: Handled search queries and submissions

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import {
   Box,
   Heading,
@@ -9,9 +9,7 @@ import {
 } from '@chakra-ui/react';
 import { FaChevronRight } from 'react-icons/fa';
 
-function SearchBar() {
-  const [ipDetails, setIpDetails] = useState({});
-
+function SearchBar({ handleSubmit, handleQuery, searchValue }) {
   return (
     <Flex
       as="header"
@@ -26,12 +24,18 @@ function SearchBar() {
       <Box display="grid" w={{ base: '80%', md: '50%' }}>
         <Heading mt="0.6em" color="white" textAlign="center" fontSize={{ base: '1.7rem' }}>IP Address Tracker</Heading>
         <InputGroup size="md" mt="8">
-          <Input w="100%" borderRadius=".7em" backgroundColor="white" placeholder="Search for any IP address or domain" border="rgb(87,116,215) solid 1px" value={ipDetails} onChange={(e) => setIpDetails(e)} />
-          <InputRightAddon border="none" backgroundColor="blackAlpha.900"><Icon color="white" as={FaChevronRight} /></InputRightAddon>
+          <Input w="100%" borderRadius=".7em" backgroundColor="white" placeholder="Search for any IP address or domain" border="rgb(87,116,215) solid 1px" value={searchValue} onChange={(e) => handleQuery(e)} />
+          <InputRightAddon border="none" backgroundColor="blackAlpha.900"><Icon color="white" as={FaChevronRight} pointerEvents onClick={handleSubmit} /></InputRightAddon>
         </InputGroup>
       </Box>
     </Flex>
   );
 }
+
+SearchBar.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleQuery: PropTypes.func.isRequired,
+  searchValue: PropTypes.string.isRequired,
+};
 
 export default SearchBar;
